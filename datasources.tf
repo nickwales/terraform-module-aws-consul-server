d5633d52ab97b5b51a46ca25c888ca16be87691e
Nick Wales <588472+nickwales@users.noreply.github.com>
2024-09-03 13:42:31 -0500

    Minor updates

--- datasources.tf
+++ datasources.tf
@@ -13,23 +13,3 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
-# data "local_file" "consul_server_key" {
-#   filename = "${path.module}/certs/${var.datacenter}-server-consul-0-key.pem"
-# }
-
-# data "local_file" "consul_server_cert" {
-#   filename = "${path.module}/certs/${var.datacenter}-server-consul-0.pem"
-# }
-
-# data "local_file" "consul_agent_ca" {
-#   filename = "${path.module}/certs/consul-agent-ca.pem"
-# }
-
-# data "local_file" "consul_encryption_key" {
-#   filename = "${path.module}/certs/encryption_key"
-# }
-
-# data "local_file" "consul_license" {
-#   filename = "${path.module}/certs/consul_license"
-# }
