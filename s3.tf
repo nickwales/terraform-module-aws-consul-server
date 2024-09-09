93e4398596eb8898f7c326d2be6f081537c3ec4b
Gerald Dykeman <gerald.dykeman@gmail.com>
2024-09-09 17:03:42 -0400

    initial attempt to catch errors, bucket only allows lowercase alphanumeric characters and hyphens

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "consul-backup-bucket" {
-  bucket = "${var.name}-consul-backup"
+  bucket = lower("${var.name}-consul-backup")
 
   tags = {
     Name = "${var.name}-consul-backup"
